65df1f78a32f32e39f22745de719bdae34a5da69
InvalidArgument3 <InvalidArgument3@gmail.com>
2024-10-19 19:23:58 -0500

    roisslinkutility

---
+++ TSTSSESLinkUtility/Data/Scripts/AdminBugReporterUtility/script.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using Sandbox.Definitions;
+using Sandbox.Game;
+using Sandbox.ModAPI;
+using SpaceEngineers.Game.ModAPI;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.ModAPI;
+using VRage.Input;
+using VRageMath;
+
+
+namespace invalid.BugReporter
+{
+    [MySessionComponentDescriptor(MyUpdateOrder.AfterSimulation)]
+    public class RoSSLinkUtility : MySessionComponentBase
+    {
+        public override void UpdateAfterSimulation()
+        {
+            if (MyAPIGateway.Utilities.IsDedicated)
+            {
+                return;
+            }
+
+            if (MyAPIGateway.Input.IsKeyPress(MyKeys.LeftShift) && MyAPIGateway.Input.IsNewKeyPressed(MyKeys.F2) && ValidInput()) //hey dumbass, use this before the url. fucking keen https://steamcommunity.com/linkfilter/?url={url}
+            {
+               
+                MyVisualScriptLogicProvider.OpenSteamOverlay("https://docs.google.com/document/d/1FywTQVZyCAt-eoNNNCeoVxqu6pSS32fvhnnBH4cN2gI/edit?usp=sharing");
+						
+            }	
+			
+			if (MyAPIGateway.Input.IsKeyPress(MyKeys.LeftControl) && MyAPIGateway.Input.IsNewKeyPressed(MyKeys.F2) && ValidInput()) //hey dumbass, use this before the url. fucking keen https://steamcommunity.com/linkfilter/?url={url}
+            {
+               
+                MyVisualScriptLogicProvider.OpenSteamOverlay("https://forms.gle/uWJQGmaJ2gTdGTFs5");
+						
+            }	
+				
+        }
+
+		public override void Init(MyObjectBuilder_SessionComponent sessionComponent)
+		{
+
+            MyAPIGateway.Utilities.ShowMessage("Server", "Press Shift + F2 to open the RoSS Infodoc" );
+            MyAPIGateway.Utilities.ShowMessage("Server", "Press Ctrl + F2 to open an issue submission form");
+			
+
+			
+		}      
+
+
+
+        private bool ValidInput()
+        {
+            if (MyAPIGateway.Session.CameraController != null && !MyAPIGateway.Gui.ChatEntryVisible && !MyAPIGateway.Gui.IsCursorVisible
+                && MyAPIGateway.Gui.GetCurrentScreen == MyTerminalPageEnum.None)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        protected override void UnloadData()
+        {
+
+        }
+    }
+}
