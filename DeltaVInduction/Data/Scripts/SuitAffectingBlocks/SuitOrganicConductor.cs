12a65e74fc21a3aa5701d87aee2be8243abf88fb
InvalidArgument3 <InvalidArgument3@gmail.com>
2024-11-25 04:30:53 -0600

    almost working debug draw beam to a character why is this so hard

--- DeltaVInduction/Data/Scripts/SuitAffectingBlocks/SuitOrganicConductor.cs
+++ DeltaVInduction/Data/Scripts/SuitAffectingBlocks/SuitOrganicConductor.cs
@@ -6,6 +6,7 @@ using Sandbox.Game;
 using VRage.Game.Components;
 using Sandbox.Common.ObjectBuilders;
 using VRageRender;
+using System.Collections.Generic;
 using VRage.Game;
 using VRage.Utils;
 
@@ -17,6 +18,7 @@ namespace SuitOrganicConductor
         private VRage.ObjectBuilders.MyObjectBuilder_EntityBase _objectBuilder;
         private const float DamageAmount = 1f;
         private IMyBeacon _conductorBlock;
+        private List<IMyCharacter> _charactersInRange = new List<IMyCharacter>();
 
         public override void Init(VRage.ObjectBuilders.MyObjectBuilder_EntityBase objectBuilder)
         {
@@ -42,65 +44,67 @@ namespace SuitOrganicConductor
             {
                 if (_conductorBlock.IsWorking)
                 {
-                    BoundingSphereD sphere = new BoundingSphereD(_conductorBlock.GetPosition(), _conductorBlock.Radius);
-                    if (Entity != null) _conductorBlock.HudText = "Dealing damage to suits...";
-                    var targetentities = MyAPIGateway.Entities.GetEntitiesInSphere(ref sphere);
-                    foreach (VRage.ModAPI.IMyEntity entity in targetentities)
-                    {
-                        var character = entity as IMyCharacter;
-                        if (character != null && !character.IsDead)
-                        {
-                            var controllingPlayer = character.ControllerInfo?.ControllingIdentityId;
-                            if (controllingPlayer.HasValue)
-                            {
-                                var playerid = controllingPlayer.Value;
-                                var health = MyVisualScriptLogicProvider.GetPlayersHealth(playerid);
-                                health -= DamageAmount;
-                                if (health <= 0)
-                                {
-                                    MyVisualScriptLogicProvider.SetPlayersHealth(playerid, 0);
-                                }
-                                else
-                                {
-                                    MyVisualScriptLogicProvider.SetPlayersHealth(playerid, health);
-                                }
-                            }
-                        }
-                    }
+                    UpdateCharactersInRange();
+                    DealDamageToCharacters();
                 }
             }
             catch { }
         }
 
+        private void UpdateCharactersInRange()
+        {
+            _charactersInRange.Clear();
+            BoundingSphereD sphere = new BoundingSphereD(_conductorBlock.GetPosition(), _conductorBlock.Radius);
+            var targetentities = MyAPIGateway.Entities.GetEntitiesInSphere(ref sphere);
+            foreach (VRage.ModAPI.IMyEntity entity in targetentities)
+            {
+                var character = entity as IMyCharacter;
+                if (character != null && !character.IsDead)
+                {
+                    _charactersInRange.Add(character);
+                }
+            }
+        }
+
+        private void DealDamageToCharacters()
+        {
+            foreach (var character in _charactersInRange)
+            {
+                var controllingPlayer = character.ControllerInfo?.ControllingIdentityId;
+                if (controllingPlayer.HasValue)
+                {
+                    var playerid = controllingPlayer.Value;
+                    var health = MyVisualScriptLogicProvider.GetPlayersHealth(playerid);
+                    health -= DamageAmount;
+                    if (health <= 0)
+                    {
+                        MyVisualScriptLogicProvider.SetPlayersHealth(playerid, 0);
+                    }
+                    else
+                    {
+                        MyVisualScriptLogicProvider.SetPlayersHealth(playerid, health);
+                    }
+                }
+            }
+        }
+
         public override void UpdateAfterSimulation()
         {
             base.UpdateAfterSimulation();
-
-            if (_conductorBlock == null) return;
-
             DrawDebugLinesToCharactersInRange();
         }
 
         private void DrawDebugLinesToCharactersInRange()
         {
-            // Only draw in a client session
-            if (!MyAPIGateway.Utilities.IsDedicated && MyAPIGateway.Session != null)
+            if (!MyAPIGateway.Utilities.IsDedicated && MyAPIGateway.Session != null && _conductorBlock != null && _conductorBlock.IsWorking)
             {
-                if (_conductorBlock == null || !_conductorBlock.IsWorking) return;
-
                 Vector3D sourcePosition = _conductorBlock.GetPosition();
-                BoundingSphereD sphere = new BoundingSphereD(sourcePosition, _conductorBlock.Radius);
-                var targetEntities = MyAPIGateway.Entities.GetEntitiesInSphere(ref sphere);
+                Vector4 red = Color.Red.ToVector4();
 
-                foreach (VRage.ModAPI.IMyEntity entity in targetEntities)
+                foreach (var character in _charactersInRange)
                 {
-                    var character = entity as IMyCharacter;
-                    if (character != null && !character.IsDead)
-                    {
-                        Vector3D characterPosition = character.GetPosition();
-                        Vector4 red = Color.Red.ToVector4();
-                        MySimpleObjectDraw.DrawLine(sourcePosition, characterPosition, MyStringId.GetOrCompute("Square"), ref red, 0.1f);
-                    }
+                    Vector3D characterPosition = character.GetPosition();
+                    MySimpleObjectDraw.DrawLine(sourcePosition, characterPosition, MyStringId.GetOrCompute("Square"), ref red, 0.1f);
                 }
             }
         }
